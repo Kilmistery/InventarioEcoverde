1873f76c3604038563b7e75ed59e119c46b5570c
John <gjohn6895@gmail.com>
2024-12-11 16:25:56 -0600

    Conexion intregrada en main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,25 +9,24 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/inventario_db";
-        String usuario = "root";
-        String contrasena = "23Ljjh12-.";//Kil
+
         //String contrasena = "1234"; //Kel
 
-        try (Connection conexion = DriverManager.getConnection(url, usuario, contrasena)){
+
+        try (Connection conexion = ConexionBD.getConnection()){
             Inventario inventario = new Inventario(conexion);
-            /*
+
             Producto producto1 = new Producto("Cobre", "A126", 20000.0, 3);
             inventario.agregarProductos(producto1);
             inventario.mostrarInvetario();
-            producto1 = new Producto(3,"Herbicida", "A043",20800.0, 4);
-            inventario.actualizarProducto(producto1);
-            */
-            if (inventario.eliminarProducto(1) == true){
+            //producto1 = new Producto(3,"Herbicida", "A043",20800.0, 4);
+            //inventario.actualizarProducto(producto1);
+
+            /*if (inventario.eliminarProducto(1) == true){
                 System.out.println("Todo bien");
             } else {
                 System.out.println("Oh no");
-            }
+            }*/
 
         } catch (SQLException e){
             System.out.println("Error al conectar a la base de datos: " + e.getMessage());
